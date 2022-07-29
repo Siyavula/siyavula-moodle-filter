37d43d5df3776f673b1b01a592014e4da7a515e4
Marli Coetzee <50846756+mareliefi@users.noreply.github.com>
2022-07-29 08:11:48 +0200

    Update version.php

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2022072000;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2022072900;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019111809;        // Requires this Moodle version
 $plugin->component = 'filter_siyavula'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
