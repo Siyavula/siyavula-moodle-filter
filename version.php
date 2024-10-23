248927782ddcc5184cd6ed45d8b4f935aa215659
Lehan Coetzee <lehan@siyavula.com>
2024-10-23 09:18:42 +0200

    Update supported Moodle versions and plugin release

--- version.php
+++ version.php
@@ -5,5 +5,5 @@ $plugin->version   = 2024102200;        // The current plugin version (Date: YYY
 $plugin->requires  = 2019111809;        // Requires this Moodle version
 $plugin->component = 'filter_siyavula'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->supported = [38, 405];
-$plugin->release = '1.3.0';
+$plugin->supported = [311, 405];
+$plugin->release = '1.4.0';
