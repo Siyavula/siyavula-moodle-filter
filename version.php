590e9a568e8f295a0fc58e8eeea255d3ef84e695
Daniel Donaldson <daniel@siyavula.com>
2025-12-03 15:44:15 +0200

    bump version

--- version.php
+++ version.php
@@ -1,9 +1,9 @@
 <?php
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2025112500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2025120300;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2019111809;        // Requires this Moodle version
 $plugin->component = 'filter_siyavula'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [311, 502];
-$plugin->release = '2.0.5';
+$plugin->release = '2.0.6';
